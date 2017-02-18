44a75d3a3600ed0a7bbcbad04584fa59349e45ec
Joh Robbins <joh.robbins@rea-group.com>
2017-02-18 23:25:23 +1100

    Made variable public for easier prototyping

--- Assets/Scripts/Orbit.cs
+++ Assets/Scripts/Orbit.cs
@@ -1,24 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Orbit : MonoBehaviour {
-
+public class Orbit : MonoBehaviour
+{
     #region member
-    private float m_orbitSpeed = 8f;
-    private Transform m_parent;
+    public float m_orbitSpeed = 8f;
+    public Transform m_parent;
     #endregion
 
     #region mono
-    // Use this for initialization
-    void Start () {
-        m_parent = this.transform.parent;
+    void Start ()
+    {
+        //m_parent = this.transform.parent;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Rotate(Vector3.up * Time.deltaTime * 3f);
 
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * 3f);
         transform.RotateAround(m_parent.transform.position, Vector3.up, m_orbitSpeed * Time.deltaTime);
     }
     #endregion
